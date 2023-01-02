a8e5dd49c66be5e8c666cfc465d8439ecb4bd0bf
safarx <ibergx00@gmail.com>
2023-01-03 02:59:14 +0545

    some loader added

--- static/main.js
+++ static/main.js
@@ -1,3 +1,9 @@
+window.addEventListener("load", function() {
+  // Hide the loader
+  document.getElementById("loader").style.display = "none";
+
+});
+
 var scrollDownButton = document.getElementById("headerButton");
 
 scrollDownButton.addEventListener("click", function() {
